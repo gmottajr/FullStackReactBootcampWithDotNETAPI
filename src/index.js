bfeb18c7fce90c3fd6f5c44fa87d88f247ca5d7e
Gerson Jr <jrmottagerson@gmail.com>
2024-01-14 06:30:32 +0000

    Initial taskopedia React project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const subHeaderStyle = {
+  color:"#ccc", 
+  backgroundColor:"blueviolet"
+}
+const MainBody = function () {
+  return (
+    <div>
+      <p>
+        In this course, we will learn React by building TaskOpedia!
+      </p>
+      <ul>
+        <li>
+          Call Ben.
+        </li>
+        <li>
+          Got to Walmart!
+        </li>
+      </ul>
+    </div>
+  );
+} 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const HeaderMain = function () {
+  return (
+    <h1>REACT COURSE</h1>
+  );
+} 
+
+const SubHeader = function () {
+  return (<h3 style={subHeaderStyle}>It is an exiting course!</h3>);
+}
+
+const HeaderGm = function () {
+  return (
+    <header>
+      <HeaderMain />
+      <SubHeader />
+    </header>
+  );
+}
+
+const FooterGm = function () {
+  return (
+    <footer>
+      <p style={{color:"#1200FF", fontStyle:"italic", backgroundColor:"#ddd"}}>Happy coding!</p>
+    </footer>
+  );
+}
+
+root.render(<div>
+  <HeaderGm />
+  <MainBody />
+  <FooterGm />
+</div>);
+
